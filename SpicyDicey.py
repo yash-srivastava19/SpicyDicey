fec647e8e4ef20c14d9c6d81dfd8d2c01f4116aa
Yash Srivastava <85068689+yash-srivastava19@users.noreply.github.com>
2021-12-09 10:51:23 +0530

    Update SpicyDicey.py

--- SpicyDicey.py
+++ SpicyDicey.py
@@ -2,6 +2,7 @@
 #Create the datasets for this model by first running "Dataset.py" (adjust path if required)
 #Use tensorboard for evaluating the model.
 import os
+import time
 import numpy
 import tensorflow as tf
 from dataclasses import dataclass
@@ -15,40 +16,44 @@ class Hyperparameters:
     INPUT_SHAPE = (28,28,1)
     BATCH_SIZE = 5
     EPOCHS = 100
-    LOGS_DIR = "/tmp/tb/tf_logs/"
+    LOGS_DIR = "/tmp/tb/tf_logs/Dice_MNIST/" + time.strftime('%d-%m-%Y_%H-%M-%S') 
 
 config = Hyperparameters()
 
 #Add your path to the numpy files here
-xTrain,yTrain = numpy.load("TrainDiceImages.npy"),numpy.load("TrainDiceLabels.npy")
+xTrain,yTrain = numpy.load('TrainDiceImages.npy'), numpy.load('TrainDiceLabels.npy')
+xTest,yTest = numpy.load('TestDiceImages.npy'), numpy.load('TestDiceLabels.npy')
 
-xTest,yTest = numpy.load("TestDiceImages.npy"),numpy.load("TestDiceLabels.npy")
-
-
-xTrain = xTrain.astype('float32')/255
 xTrain = numpy.expand_dims(xTrain,-1)
-
-xTest = xTest.astype('float32')/255
 xTest = numpy.expand_dims(xTest,-1)
-
 
 yTrain = tf.keras.utils.to_categorical(yTrain,config.CLASSES)
 yTest  = tf.keras.utils.to_categorical(yTest,config.CLASSES)
 
 # It is recommended to double check these hyperparameters by tuning(preferably by using kerastuner)
 model = tf.keras.Sequential([
-    tf.keras.layers.InputLayer(input_shape= config.INPUT_SHAPE),
-    tf.keras.layers.Conv2D(16,kernel_size=(3,3),activation='relu',padding='same'),
-    tf.keras.layers.MaxPooling2D(padding='same'),
-    tf.keras.layers.Conv2D(32,kernel_size=(1,1),activation='relu',padding='same'),
-    tf.keras.layers.MaxPooling2D(padding='same'),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dropout(0.5), 
-    tf.keras.layers.Dense(config.CLASSES,activation='softmax')
+    tf.keras.layers.InputLayer(input_shape= config.INPUT_SHAPE,name = "InputLayer"),
+    
+    tf.keras.layers.Conv2D(32,kernel_size=(3,3),activation='relu',padding='same',name = "Conv1),
+    tf.keras.layers.MaxPooling2D(padding='same',name = "MaxPool1"),
+                           
+    tf.keras.layers.Conv2D(32,kernel_size=(3,3),activation='relu',padding='same',name = "Conv2"),
+    tf.keras.layers.MaxPooling2D(padding='same',name = "MaxPool2"),                   
+    
+    tf.keras.layers.Conv2D(64,kernel_size=(3,3),activation='relu',padding='same',name = "Conv3"),
+    tf.keras.layers.MaxPooling2D(padding='same',name = "MaxPool3"),                       
+                           
+    tf.keras.layers.Flatten(name = "FlatLayer1"),
+    tf.keras.layers.Dropout(0.1,name = "Dropout1"),
+                           
+    tf.keras.layers.Dense(60,name = "Dense60")                       
+    tf.keras.layers.Dense(config.CLASSES,activation='softmax',name = "Dense6")
 
 ])
 
 #Debug: model.summary()
+#Save : tf.keras.utils.plot_model('model.png',show_dtype = False, show_shapes = False, show_layer_names = True)
+    
 tBoardCallback = keras.callbacks.TensorBoard(config.LOGS_DIR,histogram_freq = 1, profile_batch = (500,520))
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
@@ -58,3 +63,4 @@ model.fit(xTrain,yTrain,batch_size=config.BATCH_SIZE ,epochs=config.EPOCHS,callb
 loss,acc = model.evaluate(xTest,yTest)
 
 #Debug: print("Loss:{}  Accuracy:{}".format(loss,acc))
+#Save : model.save("DiceMNIST.h5")
